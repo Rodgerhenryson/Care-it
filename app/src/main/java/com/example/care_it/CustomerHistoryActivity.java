943fb4308d3b3ab9db76bf20449b745e70d843ef
kimpetertanui <kimelipeter526@gmail.com>
2020-08-29 19:54:30 +0300

    Added customer history Activity

---
+++ app/src/main/java/com/example/care_it/CustomerHistoryActivity.java
@@ -0,0 +1,54 @@
+package com.example.care_it;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class CustomerHistoryActivity extends AppCompatActivity {
+    private FloatingActionButton floatingActionButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_history);
+
+    //back press toolbar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle(R.string.your_history);
+        }
+
+        floatingActionButton=findViewById(R.id.floating_action_button);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(CustomerHistoryActivity.this,CustomerUploadActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    //back press toolbar
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+}
