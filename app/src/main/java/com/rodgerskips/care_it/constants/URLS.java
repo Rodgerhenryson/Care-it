41104ea75bfa94ed24cd86d250ea304fe0af533b
Rodgerhenryson <rodgyc@gmail.com>
2020-08-30 12:34:00 +0300

    new updates for tehnician

--- app/src/main/java/com/rodgerskips/care_it/constants/URLS.java
+++ app/src/main/java/com/rodgerskips/care_it/constants/URLS.java
@@ -1,7 +1,7 @@
 package com.rodgerskips.care_it.constants;
 
 public class URLS {
-    private static final String URL_ROOT = "http://192.168.43.199/Api.php?action=";
+    private static final String URL_ROOT = "http://192.168.8.120/Care-it/Api.php?action=";
     public static final String URL_REGISTER = URL_ROOT + "signup";
     public static final String URL_LOGIN= URL_ROOT + "login";
 }
