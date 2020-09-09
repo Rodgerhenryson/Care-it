6f60dfa58765567c33c3d2aebd179ad8c3f70132
kiduyu-klaus <muigakiduyu1995@gmail.com>
2020-09-09 16:28:58 +0300

    updated Splash

--- app/src/main/java/com/rodgerskips/care_it/activities/InitialActivity.java
+++ app/src/main/java/com/rodgerskips/care_it/activities/InitialActivity.java
@@ -1,45 +1,39 @@
 package com.rodgerskips.care_it.activities;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.rodgerskips.care_it.R;
+import com.rodgerskips.care_it.utils.Utils;
 
 public class InitialActivity extends AppCompatActivity {
     Button customer_button,technician_button;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
-        ActionBar actionBar = getSupportActionBar();
+        Utils.SetStatusBar(this);
 
 
-        customer_button=findViewById(R.id.customer_button);
-        technician_button=findViewById(R.id.technician);
 
-        customer_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-             startActivity(new Intent(InitialActivity.this, LogInActivity.class));
-            }
-        });
-
-        technician_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                startActivity(new Intent(InitialActivity.this, TechnicianLoginActivity.class));
-            }
-        });
 
     }
 
 
+    public void Register(View view) {
+        startActivity(new Intent(this,RegisterActivity.class));
+    }
+
+    public void Login(View view) {
+        startActivity(new Intent(this,LogInActivity.class));
+    }
 }
